6d2ef2b168c71a07312c4b664e1f04b54ea6151a
Mingkai Jiang <shanyujordan512@gmail.com>
2021-08-20 10:21:02 +1000

    commented and cleaned

--- Run_program_Nolan.R
+++ Run_program_Nolan.R
@@ -17,41 +17,42 @@ source("prepare.R")
 nswDF <- prepare_NSW_DFs()
 
 #### 5.1 daily rainfall
+#### takes about 10 hour to run
 for (z in 1:nrow(nswDF)) {
     
     ##### 5.1 daily rainfall
-    #convert_from_spatial_to_temporal_DF_for_user_defined_regions_rain_Nolan(sourceDir = "/Volumes/TOSHIBAEXT/AWAP/rain/", 
-    #                                                                        destDir = "input",
-    #                                                                        varName = "rain",
-    #                                                                        user.lat.max = nswDF$lat.start[z],
-    #                                                                        user.lat.min = nswDF$lat.end[z],
-    #                                                                        user.lon.min = nswDF$lon.start[z],
-    #                                                                        user.lon.max = nswDF$lon.end[z],
-    #                                                                        user.region.name = paste0("NSW", z))
-    #
-    ##### 5.2. daily Tmax
-    #convert_from_spatial_to_temporal_DF_for_user_defined_regions_tmax_Nolan(sourceDir = "/Volumes/TOSHIBAEXT/AWAP/tmax/", 
-    #                                                                        destDir = "input",
-    #                                                                        varName = "tmax",
-    #                                                                        user.lat.max = nswDF$lat.start[z],
-    #                                                                        user.lat.min = nswDF$lat.end[z],
-    #                                                                        user.lon.min = nswDF$lon.start[z],
-    #                                                                        user.lon.max = nswDF$lon.end[z],
-    #                                                                        user.region.name = paste0("NSW", z))
-    #
-    #
-    #
-    ###### 7. Calculate PET minus P
-    ###### 7.1. Calculate PET based on Tmax
-    ######      return monthly DF
-    #calculate_PET_based_on_Tmax_Nolan(sourceDir = "input",
-    #                                  destDir = "input",
-    #                                  varName = "pet",
-    #                                  user.lat.max = nswDF$lat.start[z],
-    #                                  user.lat.min = nswDF$lat.end[z],
-    #                                  user.lon.min = nswDF$lon.start[z],
-    #                                  user.lon.max = nswDF$lon.end[z],
-    #                                  user.region.name = paste0("NSW", z))
+    convert_from_spatial_to_temporal_DF_for_user_defined_regions_rain_Nolan(sourceDir = "/Volumes/TOSHIBAEXT/AWAP/rain/", 
+                                                                            destDir = "input",
+                                                                            varName = "rain",
+                                                                            user.lat.max = nswDF$lat.start[z],
+                                                                            user.lat.min = nswDF$lat.end[z],
+                                                                            user.lon.min = nswDF$lon.start[z],
+                                                                            user.lon.max = nswDF$lon.end[z],
+                                                                            user.region.name = paste0("NSW", z))
+    
+    #### 5.2. daily Tmax
+    convert_from_spatial_to_temporal_DF_for_user_defined_regions_tmax_Nolan(sourceDir = "/Volumes/TOSHIBAEXT/AWAP/tmax/", 
+                                                                            destDir = "input",
+                                                                            varName = "tmax",
+                                                                            user.lat.max = nswDF$lat.start[z],
+                                                                            user.lat.min = nswDF$lat.end[z],
+                                                                            user.lon.min = nswDF$lon.start[z],
+                                                                            user.lon.max = nswDF$lon.end[z],
+                                                                            user.region.name = paste0("NSW", z))
+    
+    
+    
+    ##### 7. Calculate PET minus P
+    ##### 7.1. Calculate PET based on Tmax
+    #####      return monthly DF
+    calculate_PET_based_on_Tmax_Nolan(sourceDir = "input",
+                                      destDir = "input",
+                                      varName = "pet",
+                                      user.lat.max = nswDF$lat.start[z],
+                                      user.lat.min = nswDF$lat.end[z],
+                                      user.lon.min = nswDF$lon.start[z],
+                                      user.lon.max = nswDF$lon.end[z],
+                                      user.region.name = paste0("NSW", z))
     
     ##### 7.2 Calculate PET minus P
     #####     return monthly DF 
@@ -82,28 +83,7 @@ compute_water_deficit_percentile_Nolan(sourceDir = "input",
 
 
 
-#nsw.lon.max = 162
-#nsw.lon.min = 138
-#nsw.lat.max = -14
-#nsw.lat.min = -38
-#
-#nsw.lat.list.s <- seq(nsw.lat.max, (nsw.lat.min+8), by=-8)
-#nsw.lon.list.s <- seq(nsw.lon.min, (nsw.lon.max-8), by=8)
-#nsw.lat.list.e <- seq((nsw.lat.max-8), nsw.lat.min, by=-8)
-#nsw.lon.list.e <- seq((nsw.lon.min+8), nsw.lon.max, by=8)
-#
-#nswDF <- data.frame("lon.start" = rep(nsw.lon.list.s, each=length(nsw.lat.list.s)),
-#                    "lon.end" = rep(nsw.lon.list.e, each=length(nsw.lat.list.e)),
-#                    "lat.start" = rep(nsw.lat.list.s, length(nsw.lon.list.s)),
-#                    "lat.end" = rep(nsw.lat.list.e, length(nsw.lon.list.e)))
-
 merge_water_deficit_dataframes_and_plot_Nolan(sourceDir="output/Nolan",
                                               destDir="output/Nolan",
                                               nswDF)
 
-### convert into 3d matrix
-#d1 <- sqrt(dim1)
-#test <- array(unlist(myData), dim = c(d1,d1,dim2))
-#test1 <- test[,,1]
-#r1 <- raster(test1)
-#plot(r1)
